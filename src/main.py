48ec6b24cc29f1316cb43efd02e4cb1e725cfd21
Kate Tacastacas <kathrinatacastacas@gmail.com>
2026-02-05 19:36:23 +0800

    edit index

--- src/main.py
+++ src/main.py
@@ -31,7 +31,29 @@ def main():
 
     for dirname, dirnames, filenames in os.walk('.'):
         if 'index.html' in filenames:
-            print("index.html already exists, skipping...")
+            print("index.html already exists, generating...")
+            with open(os.path.join(dirname, 'index.html'), 'w', encoding="utf-8") as f:
+                f.write("\n".join([
+                    get_template_head(dirname),
+                    "<tr class=\"w-2/4 bg-white border-b hover:bg-gray-50\"><th scope=\"row\" class=\" py-2 px-2 lg:px-6 font-medium text-gray-900 whitespace-nowrap flex align-middle\"><img style=\"max-width:23px; margin-right:5px\" src=\"" + get_icon_base64("o.folder-home") + "\"/>" +
+                        "<a class=\"my-auto text-blue-700\" href=\"../\">../</a></th><td>-</td><td>-</td></tr>" if dirname != "." else "",
+                        ]))
+                #sort dirnames alphabetically
+                dirnames.sort()
+                for subdirname in dirnames:
+                    f.write("<tr class=\"w-1/4 bg-white border-b hover:bg-gray-50\"><th scope=\"row\" class=\" py-2 px-2 lg:px-6 font-medium text-gray-900 whitespace-nowrap flex align-middle\"><img style=\"max-width:23px; margin-right:5px\" src=\"" + get_icon_base64("o.folder") + "\"/>" + "<a class=\"my-auto text-blue-700\" href=\"" + subdirname + "/\">" +
+                            subdirname + "/</a></th><td>-</td><td>-</td></tr>\n")
+                #sort filenames alphabetically
+                filenames.sort()
+                for filename in filenames:
+                    path = (dirname == '.' and filename or dirname +
+                            '/' + filename)
+                    f.write("<tr class=\"w-1/4 bg-white border-b hover:bg-gray-50\"><th scope=\"row\" class=\" py-2 px-2 lg:px-6 font-medium text-gray-900 whitespace-nowrap flex align-middle\"><img style=\"max-width:23px; margin-right:5px\" src=\"" + get_icon_base64(filename) + "\"/>" + "<a class=\"my-auto text-blue-700\" href=\"" + filename + "\">" + filename + "</a></th><td>" +
+                            get_file_size(path) + "</td><td>" + get_file_modified_time(path) + "</td></tr>\n")
+
+                f.write("\n".join([
+                    get_template_foot(),
+                ]))
         else:
             print("index.html does not exist, generating")
             with open(os.path.join(dirname, 'index.html'), 'w', encoding="utf-8") as f:
